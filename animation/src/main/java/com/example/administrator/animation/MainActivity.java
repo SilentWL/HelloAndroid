c85dec63d54a8498a67c593fbe1586f3b3efdd19
Administrator <lei.wang@szupai.com>
2016-08-31 16:02:44 +0800

    20160831

--- animation/src/main/java/com/example/administrator/animation/MainActivity.java
+++ animation/src/main/java/com/example/administrator/animation/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         AlphaBtn = (Button)findViewById(R.id.Alpha);
         TranslateBtn = (Button)findViewById(R.id.Translate);
         RotateBtn = (Button)findViewById(R.id.Rotate);
